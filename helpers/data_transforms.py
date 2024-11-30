0f1710768c0f6c05d413ead4230a2d6ad2e4eeb4
rmastand <radha.mastandrea@gmail.com>
2024-11-30 14:01:52 -0800

    feature plots with diff bdt cutoffs

--- helpers/data_transforms.py
+++ helpers/data_transforms.py
@@ -1,21 +1,26 @@
 import numpy as np
 
+epsilon = 1e-10
+
 def logit_transform(x, all_min, all_max):
     
     x_norm = (x-all_min)/(all_max-all_min)
     x_norm = 0.98*x_norm + 0.01
     #x_norm = x_norm[(x_norm != 0) & (x_norm != 1)]
-    logit = np.log(x_norm/(1-x_norm+1e-10)+1e-10)
+    logit = np.log(x_norm/(1.0-x_norm+epsilon)+epsilon)
     #print(np.sum(np.isnan(logit)))
     #logit = logit[~np.isnan(logit)]
     return logit
 
 
-def inverse_transform(x, preproc_info):
+def scaled_to_physical_transform(scaled_x, preproc_info):
     
-    unscaled_x =  x*preproc_info["std"] + preproc_info["mean"]
-    tmp = np.exp(unscaled_x) / (1.0 + np.exp(unscaled_x))
-    return tmp*(preproc_info["max"]-preproc_info["min"]) + preproc_info["min"]
+    unscaled_x =  scaled_x*preproc_info["std"] + preproc_info["mean"]
+    #inverse logit
+    x_norm = np.exp(unscaled_x) / (1.0 + np.exp(unscaled_x))
+    x_norm = (x_norm - 0.01) / 0.98
+    
+    return x_norm*(preproc_info["max"]-preproc_info["min"]) + preproc_info["min"]
 
 
 def clean_data(x):
